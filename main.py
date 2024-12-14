e9c50c687aaee32ab87bac91319b56f25218381e
Banton65 <banton65@gmail.com>
2024-12-14 17:19:23 -0500

    Add Buttons

--- main.py
+++ main.py
@@ -12,4 +12,31 @@ from sys import exit
 # Main Class
 class FitTracker(QWidget):
   def __init__(self):
-  
+    super().__init__()
+    
+  # init UI
+  def initUI(self):
+    self.date_box = QDateEdit()
+    self.date_box.setDate(QDate.currentDate())
+    
+    self.kal_box = QLineEdit()
+    self.kal_box_box.setPlaceholderText("Calories Burned")
+    self.distance_box = QLineEdit()
+    self.distance_box.setPlaceholderText("Distance Covered")
+    self.description = QLineEdit()
+    self.description.setPlaceholderText("Description")
+    
+    self.submit_button = QPushButton("Submit")
+    self.add_button = QPushButton("Add")
+    self.delete_button = QPushButton("Delete")
+    self.clear_button = QPushButton("Clear")
+    self.dark_mode = QCheckBox("Dark Mode")
+    
+    # Load Tables
+    
+    # Add Tables
+    
+    # Delete Tables
+    
+    # Calculate Calories
+    
